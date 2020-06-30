8821f81131f6b28bedc53026f33a36ba10d6703e
Collin Capano <collin.capano@aei.mpg.de>
2020-06-30 10:18:36 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+setup.py file for test GR with multimode waveforms plugin package
+"""
+
+from setuptools import Extension, setup, Command
+from setuptools import find_packages
+
+VERSION = '0.0.dev0'
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='testgr_multimode_waveforms',
+    version=VERSION,
+    description='Waveform hook that allows different parameters to be used '
+                'for different modes.',
+    long_description=long_description,
+    author='Collin Capano',
+    author_email='cdcapano@gmail.com',
+    keywords=['pycbc', 'signal processing', 'gravitational waves'],
+    install_requires=['pycbc', 'lalsuite'],
+    py_modules=['multimode_testgr'],
+    entry_points={
+        "pycbc.waveform.fd" : " = multimode_testgr:multimode_testgr_fd"},
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Astronomy',
+        'Topic :: Scientific/Engineering :: Physics',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    ],
+)
